34448115a48001eb41b462143f1700f2122a3fea
matthewhasan <matthewhasan1218@gmail.com>
2021-01-22 16:12:57 -0600

    added login validation middleware to login route

--- authenticate-me/backend/routes/api/session.js
+++ authenticate-me/backend/routes/api/session.js
@@ -6,9 +6,26 @@ const { User } = require('../../db/models');
 
 const router = express.Router();
 
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
+// Validating the Login
+const validateLogin = [
+    check('credential')
+        .exists({ checkFalsy: true })
+        .notEmpty()
+        .withMessage('Please provide a valid email or username.'),
+    check('password')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a password.'),
+    handleValidationErrors,
+];
+
+
 // Login
 router.post(
     '/',
+    validateLogin,
     asyncHandler(async (req, res, next) => {
         const { credential, password } = req.body;
 
