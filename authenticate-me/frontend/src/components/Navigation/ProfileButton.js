87f9236d721f0a563aec9397267573316a47dfcd
matthewhasan <matthewhasan1218@gmail.com>
2021-01-23 14:53:45 -0600

    debugging

--- authenticate-me/frontend/src/components/Navigation/ProfileButton.js
+++ authenticate-me/frontend/src/components/Navigation/ProfileButton.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from 'react-redux';
 import * as sessionActions from '../../store/session';
+import './Navigation.css';
 
 function ProfileButton({ user }) {
     const dispatch = useDispatch();
@@ -30,17 +31,18 @@ function ProfileButton({ user }) {
 
     return (
         <>
-            <button onClick={openMenu}>
+            <button onClick={openMenu} className="user-button">
                 <i className="fas fa-user-circle" />
             </button>
             {showMenu && (
-                <ul className="profile-dropdown">
-                    <li>{user.username}</li>
-                    <li>{user.email}</li>
-                    <li>
-                        <button onClick={logout}>Log Out</button>
-                    </li>
-                </ul>
+                <div className="profile-dropdown" style={{ listStyleType: "none", alignItems: "center" }}>
+                    <div>{user.username}</div>
+                    <div>{user.email}</div>
+                    <div>
+                        <br></br>
+                        <button onClick={logout} className="logout-button">Log Out</button>
+                    </div>
+                </div>
             )}
         </>
     );
