1f1ba459c690304e234aa825be40033be345a082
matthewhasan <matthewhasan1218@gmail.com>
2021-01-29 12:27:27 -0600

    changed header to main page lol

--- frontend/src/components/MainPage/index.js
+++ frontend/src/components/MainPage/index.js
@@ -23,7 +23,7 @@ const MainPage = () => {
 
     return (
     <>
-        <h1>Welcome to hell!</h1>
+        <h1>Main page!</h1>
             { pictures !== undefined && pictures.length > 1 && pictures.map((pics) => <a href={`/pictures/${pics.id}`}><img className="uploaded-picture" src={pics.imageLink} alt="uploaded" /></a>)}
     </>)
 }
